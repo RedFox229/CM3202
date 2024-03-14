8277afaf1c9788b9ada9acf0f2d3993ce881f7f4
Elliot Smith <smithe58@cardiff.ac.uk>
2024-03-14 10:45:27 +0000

    Initial upload of all code created so far in the project.

---
+++ Main.py
@@ -0,0 +1,245 @@
+# Required imports for interface and image processing
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import random
+from PIL import Image, ImageTk
+from PNRU_Extraction import main
+
+# Setting the size the image thumbnails will have
+size = 180, 180
+
+# Global store of images
+target_image = [] # This will store the Pillow 'Image' type of the target image
+image_dataset = [] # This will store all of the images in the dataset as Pillow 'Image' Types
+
+# This allows the user to select images from their file system to build the image set that will be analysed
+def open_set_sample():
+    image_dataset.clear() # Empties the storage array each time new images are selected
+    reset_sample()
+    reset_content()
+    # returns a tuple of the file paths
+    file_path = filedialog.askopenfilenames(title="Select Images To Be Analysed", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ico")])
+    if file_path:
+        display_image_sample(file_path)
+        img_dataset = list(file_path) # Converting the tuple to a list for iteration
+        for image in img_dataset:
+            image_dataset.append(Image.open(image)) # Adding the Pillow 'Image' types to a list
+    else:
+         messagebox.showerror('File Error', 'Error: Please Select Min. 1 Valid Image')
+
+# This allows the user to select a target image that wil be used for comparison
+def open_target_image():
+    target_image.clear() # Empties the storage array each time new images are selected
+    reset_target()
+    reset_content()
+    # returns a tuple of the file paths
+    file_path = filedialog.askopenfilenames(title="Select Target Images", filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif *.bmp *.ico")])
+    if file_path:
+        display_target_image(file_path)
+        img_dataset = list(file_path) # Converting the tuple to a list for iteration
+        for image in img_dataset:
+            target_image.append(Image.open(image)) # Adding the Pillow 'Image' types to a list  
+    else:
+        messagebox.showerror('Warning', 'No Image Selected (Optional)')     
+
+# All this does is choose a random sample from the image data set
+def select_random_display(file_path):
+    set_length = len(file_path)
+    if set_length > 4:
+        randints = random.sample(range(0,(len(file_path)-1)),4) # This simply selects 4 images from the dataset
+    else:
+        randints = list(range(0,set_length)) # If there are less than 4 images in the dataset this will display all of them
+    return_list = []
+    for value in randints:
+        return_list.append(file_path[value])
+    return return_list
+
+# This function merely displays the sample of the dataset
+def display_image_sample(file_path):
+    row = 2
+    col = 1
+    images = select_random_display(file_path)
+
+    reset_sample() # This will clear the displayed thumbnails
+
+    for item in images:
+        image =Image.open(item)
+        image = image.resize((size), Image.Resampling.LANCZOS)
+        image = ImageTk.PhotoImage(image)
+        label = tk.Label(random_sample_frame, image=image)
+        label.photo = image 
+        label.grid(row=row, column=col, padx=0, pady=0)
+        sample_labels.append(label)
+        col += 1
+
+# This function displays the target or control image
+def display_target_image(file_path):
+    row = 2
+    col = 1
+    images = select_random_display(file_path)
+
+    reset_target() # This will clear the displayed thumbnails
+
+    for item in images:
+        image =Image.open(item)
+        image = image.resize((size), Image.Resampling.LANCZOS)
+        image = ImageTk.PhotoImage(image)
+        label = tk.Label(target_image_frame, image=image)
+        label.photo = image 
+        label.grid(row=row, column=col, padx=0, pady=0)
+        target_labels.append(label)
+        col += 1
+
+# This function displays the target image after it has been manipulated
+def display_computed_image(images, display):
+    row = 0
+    col = 0
+
+    reset_content()
+    if len(images) > 1:
+        for item in images:
+            image = item.resize((display), Image.Resampling.LANCZOS)
+            image = ImageTk.PhotoImage(image)
+            label = tk.Label(content_frame, image=image)
+            label.photo = image 
+            label.grid(row=row, column=col, padx=0, pady=0)
+            content_labels.append(label)
+            if col == 9:
+                col = 0
+                row +=1
+            else:
+                col += 1
+    else:
+        image = images[0].resize((display), Image.Resampling.LANCZOS)
+        image = ImageTk.PhotoImage(image)
+        label = tk.Label(content_frame, image=image)
+        label.photo = image 
+        label.grid(row=row, column=col, padx=0, pady=0)
+        content_labels.append(label)
+
+# This function is used to clear the thumbnails of the random sample
+def reset_sample():
+    for label in sample_labels:
+        label.destroy()
+
+# This function is used to clear the contents of the main application frame
+def reset_content():
+    for label in content_labels:
+        label.destroy()
+
+# This function is used to clear the thumbnails of the target images sample
+def reset_target():
+    for label in target_labels:
+        label.destroy()
+
+# This is a temporary function used to test the systems ability to manipulate and display images
+def compute_test():
+    greyscale_images = []
+    if (len(target_image)>0): # Checking that there is an image selected for computing
+        for img in target_image:
+            greyscale_images.append(img.convert('L'))
+        display_computed_image(greyscale_images, (200,200))
+    else:
+        messagebox.showerror('Computation Error', 'Error: Please Select Analysis Image First') # pop up error box
+
+# This is a support function used to convert the images to greyscale
+def compute_greyscale():
+    greyscale_images = []
+    for img in target_image:
+        greyscale_images.append(img.convert('L'))
+    return greyscale_images
+
+# This  fuction meerly calls the denoising function in another module
+def denoise_target():
+    if (len(target_image)>0): # Checking that there is an image selected for computing
+        image_set = compute_greyscale()
+        # image = target_image[0]
+        # image = image.convert('L')
+        denoised = main(image_set)
+        display_computed_image(denoised, (600,600))
+    else:
+        messagebox.showerror('Computation Error', 'Error: Please Select Analysis Image First') # pop up error box
+
+# This function create all the labels used within the interface
+def create_labels():
+    random_display_label = tk.Label(random_sample_frame, text="Random Sample Of Image Set", font=("Helvetica", 12))
+    random_display_label.grid(row=0, column=1, sticky="nsew", columnspan=4) 
+
+    target_image_label = tk.Label(target_image_frame, text="Random Sample Of Target Set", font=("Helvetica", 12))
+    target_image_label.grid(row=0, column=1, sticky="nsew", columnspan=4) 
+
+# This function creates all the buttons for the interface
+def create_buttons():
+    open_analysis_set_btn = tk.Button(main_button_frame, text="Select Analysis Set", command=open_set_sample)
+    open_analysis_set_btn.grid(row=1, column=0)  
+
+    open_target_image_btn = tk.Button(main_button_frame, text="Select Analysis Image", command=open_target_image)
+    open_target_image_btn.grid(row=1, column=1)  
+
+    reset_btn = tk.Button(main_button_frame, text="Reset Selection", command=reset_all)
+    reset_btn.grid(row=1, column=2)
+
+    compute_btn = tk.Button(main_button_frame, text="Compute (Greyscale)", command=compute_test)
+    compute_btn.grid(row=1, column=3)
+
+    denoise_btn = tk.Button(main_button_frame, text="Denoise (WIP)", command=denoise_target)
+    denoise_btn.grid(row=1, column=4)
+
+# This function resets all of the thumbnails and storage lists for data selected in the program
+def reset_all():
+    for label in all_labels:
+        label.destroy() # This will just clear all of the images selected by the user
+    reset_sample()
+    reset_target()
+    reset_content()
+    target_image.clear()
+    image_dataset.clear()
+
+# Does thing for Tkinter
+all_labels = []
+sample_labels = []
+target_labels = []
+content_labels = []
+root = tk.Tk()
+
+# Set window title
+root.title("Image Analysis Prototype")
+
+# Set app to full screen on startup
+width= root.winfo_screenwidth() 
+height= root.winfo_screenheight()
+root.geometry("%dx%d" % (width, height))
+
+# Below are all of the frames used for this application
+# This is the frame for all of the buttons in the top of the application
+main_button_frame = tk.Frame(root, relief="flat", borderwidth=1)
+main_button_frame.grid(row=0, column=0, sticky = "nw", columnspan=2)
+
+# This frame will be used for the main section of the screen
+content_frame = tk.Frame(root, background="lightgray", relief="flat", borderwidth=1, highlightbackground="black", highlightthickness=2)
+content_frame.grid(row=1, column=0, sticky = "nsew", columnspan=2)
+
+# This frame is for the target image display in the lower left
+target_image_frame = tk.Frame(root, background="lightgray", padx=10, pady=10, relief="flat", borderwidth=1)
+target_image_frame.grid(row=2, column=0, sticky="sw")
+target_image_frame.columnconfigure(0, weight=1)
+target_image_frame.rowconfigure(0, weight=1)
+
+# This frame is for the random sample of images in the lower right
+random_sample_frame = tk.Frame(root, background="lightgray", padx=10, pady=10, relief="flat", borderwidth=1)
+random_sample_frame.grid(row=2, column=1, sticky="se")
+random_sample_frame.columnconfigure(0, weight=1)
+random_sample_frame.rowconfigure(0, weight=1)
+
+# Configuring the grid 
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+# General root configuration
+root.configure(bg="lightgray")
+
+# Generating Labels and buttons
+create_labels()
+create_buttons()
+
+root.mainloop()       
---
+++ PNRU_Extraction.py
@@ -0,0 +1,158 @@
+import pywt
+import pywt.data
+import numpy as np
+from PIL import Image
+from scipy.ndimage import filters
+
+return_lst = []
+fingerprint = []
+
+def denoise(image_set):
+    for image in image_set:
+        coeffs = pywt.dwt2(image, 'db4')
+        LL, (LH, HL, HH) = coeffs
+        LH = np.array(LH)
+        # print(LH)
+        HL = np.array(HL)
+        # print(HL)
+        HH = np.array(HH)
+        # print(HH)
+        image_final = np.add(LH, HL, HH)
+        return_lst.append(image_final)
+    return average_fingerprint(return_lst)
+
+def average_fingerprint(fingerprint_list):
+    hold = np.array(fingerprint_list[0])
+    for img_rep in fingerprint_list[1:]:
+        x = np.array(img_rep)
+        hold = np.add(hold,x)
+    average = np.divide(hold, len(return_lst))
+
+    print("Average: ")
+    checksum = 0
+    for i in average:
+        checksum += sum(i)
+    print(checksum)
+    print(sum(average))
+    returned_image = Image.fromarray(average, mode='L')
+    fingerprint.append(returned_image)
+    #return fingerprint
+
+def main(image_set):
+    return_lst.clear()
+    fingerprint.clear()
+
+    for image in image_set:
+        img = np.asarray(image)
+        return_lst.append(noise_extract(img))
+
+    average_fingerprint(return_lst)
+    return fingerprint
+
+def noise_extract(im: np.ndarray, levels: int = 4, sigma: float = 5) -> np.ndarray:
+    """
+    NoiseExtract as from Binghamton toolbox.
+
+    :param im: grayscale or color image, np.uint8
+    :param levels: number of wavelet decomposition levels
+    :param sigma: estimated noise power
+    :return: noise residual
+    """
+
+    assert (im.dtype == np.uint8)
+    assert (im.ndim in [2, 3])
+
+    im = im.astype(np.float32)
+
+    noise_var = sigma ** 2
+
+    if im.ndim == 2:
+        im.shape += (1,)
+
+    W = np.zeros(im.shape, np.float32)
+
+    for ch in range(im.shape[2]):
+
+        wlet = None
+        while wlet is None and levels > 0:
+            try:
+                wlet = pywt.wavedec2(im[:, :, ch], 'db4', level=levels)
+            except ValueError:
+                levels -= 1
+                wlet = None
+        if wlet is None:
+            raise ValueError('Impossible to compute Wavelet filtering for input size: {}'.format(im.shape))
+
+        wlet_details = wlet[1:]
+
+        wlet_details_filter = [None] * len(wlet_details)
+        # Cycle over Wavelet levels 1:levels-1
+        for wlet_level_idx, wlet_level in enumerate(wlet_details):
+            # Cycle over H,V,D components
+            level_coeff_filt = [None] * 3
+            for wlet_coeff_idx, wlet_coeff in enumerate(wlet_level):
+                level_coeff_filt[wlet_coeff_idx] = wiener_adaptive(wlet_coeff, noise_var)
+            wlet_details_filter[wlet_level_idx] = tuple(level_coeff_filt)
+
+        # Set filtered detail coefficients for Levels > 0 ---
+        wlet[1:] = wlet_details_filter
+
+        # Set to 0 all Level 0 approximation coefficients ---
+        wlet[0][...] = 0
+
+        # Invert wavelet transform ---
+        wrec = pywt.waverec2(wlet, 'db4')
+        try:
+            W[:, :, ch] = wrec
+        except ValueError:
+            W = np.zeros(wrec.shape[:2] + (im.shape[2],), np.float32)
+            W[:, :, ch] = wrec
+
+    if W.shape[2] == 1:
+        W.shape = W.shape[:2]
+
+    W = W[:im.shape[0], :im.shape[1]]
+
+    print(type(W))
+
+    return W
+
+def wiener_adaptive(x: np.ndarray, noise_var: float, **kwargs) -> np.ndarray:
+    """
+    WaveNoise as from Binghamton toolbox.
+    Wiener adaptive flter aimed at extracting the noise component
+    For each input pixel the average variance over a neighborhoods of different window sizes is first computed.
+    The smaller average variance is taken into account when filtering according to Wiener.
+    :param x: 2D matrix
+    :param noise_var: Power spectral density of the noise we wish to extract (S)
+    :param window_size_list: list of window sizes
+    :return: wiener filtered version of input x
+    """
+    window_size_list = list(kwargs.pop('window_size_list', [3, 5, 7, 9]))
+
+    energy = x ** 2
+
+    avg_win_energy = np.zeros(x.shape + (len(window_size_list),))
+    for window_idx, window_size in enumerate(window_size_list):
+        avg_win_energy[:, :, window_idx] = filters.uniform_filter(energy,
+                                                                  window_size,
+                                                                  mode='constant')
+
+    coef_var = threshold(avg_win_energy, noise_var)
+    coef_var_min = np.min(coef_var, axis=2)
+
+    x = x * noise_var / (coef_var_min + noise_var)
+
+    return x
+
+
+def threshold(wlet_coeff_energy_avg: np.ndarray, noise_var: float) -> np.ndarray:
+    """
+    Noise variance theshold as from Binghamton toolbox.
+    :param wlet_coeff_energy_avg:
+    :param noise_var:
+    :return: noise variance threshold
+    """
+    res = wlet_coeff_energy_avg - noise_var
+    return (res + np.abs(res)) / 2
+
